eaa668e66fbf877526a487ecc4da878cd141e17f
Ben Cardell <benc030602@gmail.com>
2022-06-07 14:27:35 +0100

    moved html pages into static folder

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const server = express()
 const port = 3000
 
 server.listen(port, ()=> console.log('listening'))
+
+server.get("/", (req,res) => {
+    res.sendFile(__dirname + "/static/signUp.html")
+})
