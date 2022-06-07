7f15d148042573d838f89b6a1c7c40f0dd35d721
Ben Cardell <benc030602@gmail.com>
2022-06-07 14:33:02 +0100

    added bodyParser to correct html/css issues when viewing page

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const server = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-server.listen(port, ()=> console.log('listening'))
+server.use(bodyParser.urlencoded({ extended: false}))
+server.use(express.static(__dirname));
+server.use(express.urlencoded({ extended: true}))
 
 server.get("/", (req,res) => {
     res.sendFile(__dirname + "/static/signUp.html")
 })
+
+
+
+
+
+
+
+
+server.listen(port, ()=> console.log('listening'))
