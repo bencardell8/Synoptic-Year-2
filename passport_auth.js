5c2a70b681c1f6b6fdd8dba2ccd2c11dc80728be
Tommy <35144580+TommyAK@users.noreply.github.com>
2022-06-13 16:52:34 +0100

    Implemented passport.js middleware

--- passport_auth.js
+++ passport_auth.js
@@ -1,30 +1,63 @@
-
-/*
 const LocalStrategy = require('passport-local').Strategy
+const {Client} = require ('pg')
 
-function startAuthentication(passport, getUserEmail) {
+/* storing database information */
+const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "synopticDB",
+    password: "password",
+    port: 5432,
+});
 
-    const authenticateUser = (email, password, done) => {
-        const user = getUserEmail(email)
-        if (user == null) {
-            return done(null, false, {message: "No user in DB found."})
-        }
+/* function for initialisation of passport */
+function startAuthentication(passport) {
+    console.log("Auth started")
+    /* connect to the database defined earlier */
+    client.connect();
 
-        try {
-            if ()
-            
-        } catch (error) {
-            
-        }
-
+    const authUser = (email, password, done) =>{
+        /* query to check for user */
+        client.query(`SELECT * FROM users WHERE email = $1`, [email], (err, result) =>{
+            /* throw error if present */
+            if(err) {
+                throw err;
+            }
+            /* check if a database entry exists */
+            if (result.rows.length > 0) {
+                /* store user details */
+                const user = result.rows[0]
+                /* compare entered password to actual password in database */
+                if (user.password === password) {
+                    console.log(`User ${user.email} logged in.`);
+                    return done(null, user)
+                }
+                /* check if passwords do not match print error */
+                if (user.password != password) {
+                    console.log("Invalid password.")
+                    return done(null, false, {message: "Incorrect password."})
+                }             
+                /* user does not exist in database */
+            } else {
+                console.log("User not found in database.")
+                return done(null, false, {message: "User not found in database."})
+            }
+        })
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'}), authenticateUser)
-    passport.serialiseUser((user, done) => {})
-    passport.deserialiseUser((id, done) => {})
+    passport.use(new LocalStrategy({usernameField: "email", passwordField: "password"}, authUser));
+
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    passport.deserializeUser((id, done) => {
+        client.query(`SELECT * FROM users WHERE id = $1`, [id], (err, result) => {
+            if (err) {
+                return done(err);
+            }
+            return done(null, result.rows[0]);
+        })
+    })
 
 }
 
-modules.export
-
-*/
+module.exports = startAuthentication;
--- server.js
+++ server.js
@@ -4,77 +4,70 @@ const port = 3000
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const {Client} = require ('pg')
+const passport = require('passport')
+const session = require("express-session");
+const flash = require("express-flash");
 
 server.use(bodyParser.urlencoded({ extended: false}))
 server.use(express.static(__dirname));
 server.use(express.urlencoded({ extended: true}))
 server.set("view engine", "ejs");
 
-/*
-const startAuthentication = require('./passport_auth')
-const passport = require('passport')
-startAuthentication(passport)
-*/
+
+/* initialisation function for passport.js */ 
+const startAuthentication = require('./passport_auth');
+startAuthentication(passport);
 
 
-server.get("/", (req,res) => {
+server.use(session({
+    secret: 'secure_secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// initialisation of session, passport, and flash
+server.use(passport.initialize());
+server.use(passport.session())
+server.use(flash());
+
+server.get("/", checkAuthenticated, (req,res) => {
     res.render('signup.ejs', {error: ""});
 })
 
-server.get("/login", (req,res) => {
-    res.render('login.ejs', {error: ""});
+server.get("/login", checkAuthenticated, (req,res) => {
+    res.render('login.ejs');
 })
 
-
-server.get("/account", (req,res) => {
-    res.render('account-info.ejs', {error: ""});
+server.get("/account", checkNotAuthenticated, (req,res) => {
+    res.render('account-info.ejs', {user: req.user.email});
 })
 
-/*
-server.post("/", (req, res) => {
-    let email = req.body.email
-    let password = req.body.password
-    res.setHeader('Content-type','text/html')
-
-    //console.log("Data has been saved: Email = " + email + ", Password = " + password)
-
-    console.log("Data has been saved: " + JSON.stringify(req.body))
-
-    var loginDetails = Date.now()
-    loginDetails += ".txt"
-
-    fs.appendFile(loginDetails, JSON.stringify(req.body), function(err){
-        if (err) throw err;
-        console.log('Data Saved')
-    })
-
-    res.redirect("/about")
-})
-*/
-/*
-server.get("/login", (req,res) => {
-    res.sendFile(__dirname + "/static/login.html")
-})
-*/
-server.get("/about", (req,res) => {
-    res.sendFile(__dirname + "/static/about.html")
-})
-/*
-server.get("/account", (req,res) => {
-    res.sendFile(__dirname + "/static/account-info.html")
-})
-*/
-server.get("/contact", (req,res) => {
-    res.sendFile(__dirname + "/static/contact.html")
+server.get("/editaccount", checkNotAuthenticated , (req,res) => {
+    res.render('account-edit.ejs', {user: req.user.email})
 })
 
-server.get("/editaccount", (req,res) => {
-    res.sendFile(__dirname + "/static/account-edit.html")
+server.get("/about", checkNotAuthenticated , (req,res) => {
+    res.render('about.ejs')
 })
 
+server.get("/contact", checkNotAuthenticated , (req,res) => {
+    res.render('contact.ejs')
+})
 
+/* upon logout the session is terminated and user is redirected to login page */
+server.get("/logout", (req,res) => {
+    req.logout(req.user, err => {
+        if(err) return next(err);
+        req.flash("success_msg", "Logged out!")
+        res.render('login.ejs')
+    });
+    
+})
+
+/* post method for a new user creating an account */
 server.post("/saveUser", (req,res) => {
 
+    /* postgres database information */
     const client = new Client({
         user: "postgres",
         host: "localhost",
@@ -83,116 +76,58 @@ server.post("/saveUser", (req,res) => {
         port: 5432,
     });
 
+    /* connecting to a postgres database */
     client.connect();
 
-        
+    /* sql query containing user's information */
     const insertData = `INSERT INTO users (email, password)
                 VALUES ('${req.body.email}', '${req.body.password}')`;
 
+    /* testing query against database */
     client.query(insertData, (err) => {
+        /* if the email already exists an error is displayed */
         if (err) {
             console.error(err);
             console.log("Error likely caused by duplicate email.")
             client.end();
-            res.render('signup.ejs', {error: `Error: Email '${req.body.email}' already registered.`})
-
+            res.render('signup.ejs', {error: `Email '${req.body.email}' already registered.`})
+        /* if the email is not present in the database, the account is added */
         } else {
             console.log('Data saved to database.');
             client.end();
-            res.redirect("/contact")
+            req.flash("success_msg", "Account created! Please log in.")
+            res.redirect("/login")
         }
     });
 
 })
 
-server.post("/loginUser", (req, res) => {
-
-    const client = new Client({
-        user: "postgres",
-        host: "localhost",
-        database: "synopticDB",
-        password: "password",
-        port: 5432,
-    });
-
-    client.connect();
+ /* passport.js middleware for authetication requests. */
+server.post("/loginUser", passport.authenticate("local", {
+    successRedirect: '/account',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
 
 
-    const loginData = `SELECT * FROM users WHERE email = '${req.body.email}'
-                        AND password = '${req.body.password}'`;
+/* check if a user is authenticated and redirects them back to their account page */
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/account");
+    }
+    next();
+  }
+  
+/* if a user is not authenticated they are redirected back to the login page */
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+  
 
-    console.log(loginData)
-    
-    client.query('SELECT email, password FROM users WHERE email=$1', [req.body.email], (err, result) => {
-        if (err) {
-            console.error(err);
-            console.log("Error likely caused by incorrect login details.")
-            client.end();
-            res.render('login.ejs', {error: 'Error: Incorrect login details.'})
-            /* checking if user data present in DB */
-        } if (result.rows.length > 0) {
-            /* finding the valid password for that user */
-            const checkPass = result.rows[0].password
-            /* checking if password entered matches actual password */
-            if (checkPass === req.body.password) {
-                console.log(`User ${req.body.email} logged in.`);
-                client.end();
-                res.render('account-info.ejs', {userEmail: result.rows[0].email})
-            } else {
-                client.end();
-                res.render('login.ejs', {error: 'Error: Incorrect login details.'})
-                console.log(`Invalid password for ${req.body.email}`)
-            }
-        } else {
-            client.end();
-            res.render('login.ejs', {error: 'Error: Incorrect login details.'})
-            console.log(`User ${req.body.email} not valid.`)
-
-        }
-    });
-
-})
-
-
-
-
-/*server.post("/load_from_db", function(req,res){
-    var body = ""
-    req.on("data", function(data){
-        body+=data;
-    })
-
-    var json = JSON.parse(body)
-    var email = json.email;
-
-    req.on("end", async function(){
-        const client = new Client({
-            user: "postgres",
-            host: "localhost",
-            database: "synopticDB",
-            password: "password",
-            port: 5432,
-        })
-        await client.connect()
-        await client.query("SET search_path TO 'public';")
-
-        const text = "SELECT password FROM users WHERE email = $1;"
-        const values = id;
-        const res1 = await client.query (text, values)
-        console.log(rows1)
-
-        var result = rows1.email
-
-        res.send(result)
-
-        await client.end()
-    })
-})
-*/
-
-
-
-
+/* displays server address and port in console */
 server.listen(port, ()=> {
     console.log(`Listening to http://localhost:${port}`)
 }); 
